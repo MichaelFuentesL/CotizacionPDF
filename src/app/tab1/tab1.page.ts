f8957826fbb181dc970c1507679c191b9fcfa6f5
michael.fuentes <michaelfuenteslopez1@gmail.com>
2020-02-26 02:35:25 -0300

    primer commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+    private cotiForm: FormGroup;
 
-  constructor() {}
+    constructor() {
+        this.cotiForm = new FormGroup({
+            business: new FormControl('', [Validators.required]),
+            date: new FormControl('', [Validators.required]),
+            detail1: new FormControl('', [Validators.required]),
+            time1: new FormControl('', [Validators.required]),
+            price1: new FormControl('', [Validators.required]),
+            tax: new FormControl('', [Validators.required]),
+            projectDetail: new FormControl('', [Validators.required]),
+            terms: new FormControl('', [Validators.required]),
+            initial: new FormControl('', [Validators.required])
+        });
 
+    }
+
+    addDetail(){
+alert('agregamos nuevo detalle');
+
+
+    }
 }
